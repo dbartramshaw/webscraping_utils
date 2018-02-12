416bee3c6d756c298da91c48e4f80511ebdcc860
Bartram-Shaw, David <david.bartram-shaw@outlook.com>
2018-02-12 00:29:18 +0000

    timeout exceptions added

--- selenium/screen_shot_page.py
+++ selenium/screen_shot_page.py
@@ -5,11 +5,11 @@
 """ A couple of utils to take tailored screengrabs using selenium
 """
 
-
 import pandas as pd
 from selenium import webdriver
 import datetime
 import os
+import base64
 
 '''
 ## driver setup in terminal
@@ -25,6 +25,49 @@ csv_path='/Users/bartramshawd/Documents/DBS/python_code/webscrape_utils/selenium
 urls = pd.read_csv(csv_path+'example_urls.csv')
 urls.head()
 
+# added timeout for page load (15secs)
+def driver_screenshot_timeout(url_list, folder_name='screenshots'):
+    import pandas as pd
+    import signal
+    from selenium import webdriver
+    from selenium.common.exceptions import TimeoutException
+    print 'RUNNING ...'
+    start = datetime.datetime.now()
+    save_path = csv_path+folder_name
+    if not os.path.exists(save_path):
+            os.makedirs(save_path)
+    global errors
+
+    ### Firefox
+    driver = webdriver.Firefox(executable_path = '/usr/local/bin/geckodriver')
+    #driver.execute_script("document.body.style.zoom='75%'")
+    driver.set_window_size(1500, 2500)
+    errors=[]
+    for i in range(0,len(url_list)):
+        try:
+            iter_start = datetime.datetime.now()
+            driver.set_page_load_timeout(15)
+            driver.get(url_list[i])
+            # screenshot = driver.save_screenshot(save_path+'/'+str(i)+'.png')
+            screenshot = driver.save_screenshot(csv_path+'/'+base64.b64encode(url_list[i])+'.png')
+            print str(i)+ '  saved in '+str(datetime.datetime.now()-iter_start)+' seconds'
+
+        except TimeoutException as ex:
+            errors.append(url_list[i])
+            print(i, "Exception has been thrown. " + str(ex))
+        except:
+            print(i, "other Exception has been thrown. " + str(ex))
+            pass
+    driver.quit()
+    print 'COMPLETE: '+str(len(url_list))+' Screenshots saved in '+str(datetime.datetime.now()-start)+' seconds'
+    print 'LOCATION: '+save_path
+
+
+## Run it
+driver_screenshot(urls.url[0:10], folder_name='driver_screenshots')
+
+
+
 def driver_screenshot(url_list, folder_name='screenshots'):
     import pandas as pd
     from selenium import webdriver
@@ -33,7 +76,7 @@ def driver_screenshot(url_list, folder_name='screenshots'):
     save_path = csv_path+folder_name
     if not os.path.exists(save_path):
             os.makedirs(save_path)
-
+    global errors
     ### Chrome
     #DRIVER = '/Users/bartramshawd/bin/chromedriver'
     #driver = webdriver.Chrome(DRIVER)
@@ -41,19 +84,25 @@ def driver_screenshot(url_list, folder_name='screenshots'):
     ### Firefox
     driver = webdriver.Firefox(executable_path = '/usr/local/bin/geckodriver')
     #driver.execute_script("document.body.style.zoom='75%'")
-
     driver.set_window_size(1000, 2000)
+
+    errors=[]
     for i in range(0,len(url_list)):
-        iter_start = datetime.datetime.now()
-        driver.get(url_list[i])
-        screenshot = driver.save_screenshot(save_path+'/'+str(i)+'.png')
-        print str(i)+ '  saved in '+str(datetime.datetime.now()-iter_start)+' seconds'
+        signal.alarm(15)
+        try:
+            iter_start = datetime.datetime.now()
+            driver.get(url_list[i])
+            # screenshot = driver.save_screenshot(save_path+'/'+str(i)+'.png')
+            screenshot = driver.save_screenshot(save_path+'/'+base64.b64encode(page_urls[i])+'.png')
+            print str(i)+ '  saved in '+str(datetime.datetime.now()-iter_start)+' seconds'
+        except TimeoutException:
+            errors.append(i)
+            print('TIMEOUT 15secs:',i)
+            continue
     driver.quit()
     print 'COMPLETE: '+str(len(url_list))+' Screenshots saved in '+str(datetime.datetime.now()-start)+' seconds'
     print 'LOCATION: '+save_path
 
-## Run it
-driver_screenshot(urls.url[0:10], folder_name='driver_screenshot_examples')
 
 
 def phantomJS_screenshot(url_list, folder_name='test_folder'):
